912cc12994a3f9faa53bc11691cd7bbb6227d31c
Luiz Felipe <103139855+LuizSdO@users.noreply.github.com>
2023-11-22 23:15:59 -0300

    procedures

--- DermaHelp_Global_Solution.sql
+++ DermaHelp_Global_Solution.sql
@@ -1,52 +1,264 @@
-CREATE OR REPLACE PROCEDURE carregar_endereco
-IS
-    v_ds_logradouro endereco.ds_logradouro%TYPE;
-    v_ds_complemento endereco.ds_complemento%TYPE;
-    v_nm_cidade endereco.nm_cidade%TYPE;
-    v_nm_estado endereco.nm_estado%TYPE;
-    v_ds_cep endereco.ds_cep%TYPE;
+set serveroutput on
+set verify off
 
-    -- ExceÃ§Ã£o personalizada 1
-    DUP_VAL_EXCEPTION EXCEPTION;
-    PRAGMA EXCEPTION_INIT(DUP_VAL_EXCEPTION, -1);
 
-    -- ExceÃ§Ã£o personalizada 2
-    INVALID_CITY_EXCEPTION EXCEPTION;
-    PRAGMA EXCEPTION_INIT(INVALID_CITY_EXCEPTION, -2);
+--------------------PROCEDURE INSERT PARA ENDERECO-----------------------------
+CREATE OR REPLACE PROCEDURE carregar_enderecos AS
+BEGIN
+  INSERT INTO endereco (id_endereco, ds_logradouro, ds_complemento, nm_cidade, nm_estado, ds_cep)
+  VALUES (1,'Rua Augusta', 'Sala 203', 'São Paulo', 'SP', '01305-000');
+  
+  INSERT INTO endereco (id_endereco, ds_logradouro, ds_complemento, nm_cidade, nm_estado, ds_cep)
+  VALUES (2,'Avenida Boa Viagem', 'Apto 501', 'Recife', 'PE', '51021-000');
 
-    -- ExceÃ§Ã£o personalizada 3
-    INVALID_STATE_EXCEPTION EXCEPTION;
-    PRAGMA EXCEPTION_INIT(INVALID_STATE_EXCEPTION, -3);
+  INSERT INTO endereco (id_endereco, ds_logradouro, ds_complemento, nm_cidade, nm_estado, ds_cep)
+  VALUES (3,'Rua das Palmeiras', 'Casa 15', 'Belo Horizonte', 'MG', '30112-020');
+
+  INSERT INTO endereco (id_endereco, ds_logradouro, ds_complemento, nm_cidade, nm_estado, ds_cep)
+  VALUES (4,'Avenida Paulista', 'Andar 10', 'São Paulo', 'SP', '01311-000');
+
+  INSERT INTO endereco (id_endereco, ds_logradouro, ds_complemento, nm_cidade, nm_estado, ds_cep)
+  VALUES (5,'Rua da Consolação', 'Loja 3', 'São Paulo', 'SP', NULL);
+
+EXCEPTION
+  WHEN DUP_VAL_ON_INDEX THEN
+    INSERT INTO t_erro (nm_erro, dt_ocorrencia, nm_usuario)
+    VALUES ('Duplicidade de chave primária ao inserir endereço', SYSDATE, USER);
+
+  WHEN OTHERS THEN
+    IF SQLCODE = -1400 THEN
+      INSERT INTO t_erro (nm_erro, dt_ocorrencia, nm_usuario)
+      VALUES ('Violacao de NOT NULL ao inserir endereço', SYSDATE, USER);
+    ELSE
+      INSERT INTO t_erro (nm_erro, dt_ocorrencia, nm_usuario)
+      VALUES ('Erro desconhecido ao inserir endereço', SYSDATE, USER);
+    END IF;
+END carregar_enderecos;
 
 BEGIN
-    FOR i IN 1..10
-    LOOP
-        v_ds_logradouro := 'Rua ' || TO_CHAR(i);
-        v_ds_complemento := 'Complemento ' || TO_CHAR(i);
-        v_nm_cidade := CASE WHEN i <= 5 THEN 'Valid City' ELSE 'Invalid City' END;
-        v_nm_estado := CASE WHEN i <= 5 THEN 'Valid State' ELSE 'Invalid State' END;
-        v_ds_cep := '1234567' || TO_CHAR(i);
+  carregar_enderecos;
+END;
 
-        BEGIN
-            INSERT INTO endereco (ds_logradouro, ds_complemento, nm_cidade, nm_estado, ds_cep)
-            VALUES (v_ds_logradouro, v_ds_complemento, v_nm_cidade, v_nm_estado, v_ds_cep);
 
-            COMMIT;
+select * from endereco;
+SELECT * FROM t_erro;
 
-        EXCEPTION
-            WHEN DUP_VAL_EXCEPTION THEN
-                INSERT INTO t_erro (cd_erro, nm_erro, dt_ocorrencia, nm_usuario)
-                VALUES (-1, 'Duplicated Value', SYSDATE, USER);
-            WHEN INVALID_CITY_EXCEPTION THEN
-                INSERT INTO t_erro (cd_erro, nm_erro, dt_ocorrencia, nm_usuario)
-                VALUES (-2, 'Invalid City', SYSDATE, USER);
-            WHEN INVALID_STATE_EXCEPTION THEN
-                INSERT INTO t_erro (cd_erro, nm_erro, dt_ocorrencia, nm_usuario)
-                VALUES (-3, 'Invalid State', SYSDATE, USER);
-            WHEN OTHERS THEN
-                INSERT INTO t_erro (cd_erro, nm_erro, dt_ocorrencia, nm_usuario)
-                VALUES (-999, SQLERRM, SYSDATE, USER);
-        END;
+
+--------------------PROCEDURE INSERT PARA CONSULTORIO-----------------------------
+CREATE OR REPLACE PROCEDURE carregar_consultorios AS
+BEGIN
+  INSERT INTO consultorio (id_consultorio, id_endereco, nm_empresa, nr_cnpj)
+  VALUES (1, 1, 'Clínica ABC', '12345678901234');
+
+  INSERT INTO consultorio (id_consultorio, id_endereco, nm_empresa, nr_cnpj)
+  VALUES (2, 2, 'Hospital XYZ', '56789012345678');
+
+  INSERT INTO consultorio (id_consultorio, id_endereco, nm_empresa, nr_cnpj)
+  VALUES (3, 3, 'Centro Médico DEF', '90123456789012');
+  
+  INSERT INTO consultorio (id_consultorio, id_endereco, nm_empresa, nr_cnpj)
+  VALUES (4, 4, 'Clínica XYZ', '34567890123456');
+
+  INSERT INTO consultorio (id_consultorio, id_endereco, nm_empresa, nr_cnpj)
+  VALUES (5, 5, 'Hospital ABC', '67890123456789');
+  
+  INSERT INTO consultorio (id_consultorio, id_endereco, nm_empresa, nr_cnpj)
+  VALUES (6, 6, 'Centro Médico GHI', '01234567890123');
+
+  INSERT INTO consultorio (id_consultorio, id_endereco, nm_empresa, nr_cnpj)
+  VALUES (7, 7, 'Clínica Delta', '12345098765432');
+
+EXCEPTION
+  WHEN DUP_VAL_ON_INDEX THEN
+    INSERT INTO t_erro (nm_erro, dt_ocorrencia, nm_usuario)
+    VALUES ('Duplicidade de chave primária ao inserir consultório', SYSDATE, USER);
+
+  WHEN VALUE_ERROR THEN
+    INSERT INTO t_erro (nm_erro, dt_ocorrencia, nm_usuario)
+    VALUES ('Erro de valor ao inserir consultório (por exemplo, NOT NULL violado)', SYSDATE, USER);
+
+  WHEN OTHERS THEN
+    INSERT INTO t_erro (nm_erro, dt_ocorrencia, nm_usuario)
+    VALUES ('Erro desconhecido ao inserir consultório', SYSDATE, USER);
+END carregar_consultorios;
+
+BEGIN
+  carregar_consultorios;
+END;
+
+SELECT * FROM t_erro;
+SELECT * FROM CONSULTORIO;
+
+
+-----------------------PROCEDURE INSERT PARA USUARIO----------------------------
+CREATE OR REPLACE PROCEDURE carregar_usuarios AS
+  v_erro_msg VARCHAR2(255);
+BEGIN
+  BEGIN
+    INSERT INTO usuario (id_usuario, nm_usuario, nr_cpf, ds_email, ds_senha, ds_genero)
+    VALUES (1, 'João Silva', '12345678901', 'joao.silva@email.com', 'senha123', 'M');
+
+    INSERT INTO usuario (id_usuario, nm_usuario, nr_cpf, ds_email, ds_senha, ds_genero)
+    VALUES (2, 'Maria Oliveira', '23456789012', 'maria.oliveira@email.com', 'senha456', 'F');
+
+    INSERT INTO usuario (id_usuario, nm_usuario, nr_cpf, ds_email, ds_senha, ds_genero)
+    VALUES (3, 'Carlos Santos', '34567890123', 'carlos.santos@email.com', 'senha789', 'M');
+
+    INSERT INTO usuario (id_usuario, nm_usuario, nr_cpf, ds_email, ds_senha, ds_genero)
+    VALUES (4, 'Ana Pereira', '45678901234', 'ana.pereira@email.com', 'senhaABC', 'F');
+
+    INSERT INTO usuario (id_usuario, nm_usuario, nr_cpf, ds_email, ds_senha, ds_genero)
+    VALUES (5, 'Paulo Oliveira', '56789012345', 'paulo.oliveira@email.com', 'senhaDEF', 'M');
+
+    INSERT INTO usuario (id_usuario, nm_usuario, nr_cpf, ds_email, ds_senha, ds_genero)
+    VALUES (6, 'Mariana Santos', '67890123456', 'mariana.santos@email.com', 'senhaGHI', 'F');
+
+    INSERT INTO usuario (id_usuario, nm_usuario, nr_cpf, ds_email, ds_senha, ds_genero)
+    VALUES (7, 'Pedro Silva', '78901234567', 'pedro.silva@email.com', 'senhaJKL', 'M');
+    
+    INSERT INTO usuario (id_usuario, nm_usuario, nr_cpf, ds_email, ds_senha, ds_genero)
+  VALUES (8, NULL, '12345678901', 'teste@email.com', 'senha123', 'M');
+  
+  EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+      v_erro_msg := 'Duplicidade de chave primária ao inserir usuário';
+    WHEN VALUE_ERROR THEN
+      v_erro_msg := 'Erro ao inserir usuário (por exemplo, NOT NULL violado)';
+    WHEN NO_DATA_FOUND THEN
+      v_erro_msg := 'Nenhum dado encontrado ao inserir usuário';
+    WHEN TOO_MANY_ROWS THEN
+      v_erro_msg := 'Múltiplos registros encontrados ao inserir usuário';
+    WHEN OTHERS THEN
+      v_erro_msg := 'Erro desconhecido ao inserir usuário: ' || SQLERRM;
+  END;
+
+  IF v_erro_msg IS NOT NULL THEN
+    v_erro_msg := SUBSTR(v_erro_msg, 1, 200);
+    INSERT INTO t_erro (nm_erro, dt_ocorrencia, nm_usuario)
+    VALUES (v_erro_msg, SYSDATE, USER);
+  END IF;
+END carregar_usuarios;
+
+
+BEGIN
+  carregar_usuarios;
+END;
+
+SELECT * FROM t_erro;
+SELECT * FROM usuario;
+
+
+-----------------------PROCEDURE INSERT PARA IMAGEM----------------------------
+CREATE OR REPLACE PROCEDURE carregar_imagens AS
+  v_erro_msg VARCHAR2(255);
+BEGIN
+  BEGIN
+    FOR i IN 1..7 LOOP
+      INSERT INTO imagem (id_imagem, id_usuario, dt_data_hora, ds_resultado)
+      VALUES (i, MOD(i, 3) + 1, SYSTIMESTAMP, 'Resultado ' || TO_CHAR(i));
     END LOOP;
-END carregar_endereco;
+  EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+      v_erro_msg := 'Duplicidade ao inserir imagem';
+    WHEN VALUE_ERROR THEN
+      v_erro_msg := 'Erro ao inserir imagem (por exemplo, NOT NULL violado)';
+    WHEN NO_DATA_FOUND THEN
+      v_erro_msg := 'Nenhum dado encontrado ao inserir imagem';
+    WHEN OTHERS THEN
+      v_erro_msg := 'Erro desconhecido ao inserir imagem';
+  END;
 
+  IF v_erro_msg IS NOT NULL THEN
+    v_erro_msg := SUBSTR(v_erro_msg, 1, 50);
+    INSERT INTO t_erro (nm_erro, dt_ocorrencia, nm_usuario)
+    VALUES (v_erro_msg, SYSDATE, USER);
+  END IF;
+
+  BEGIN
+    INSERT INTO imagem (id_imagem, id_usuario, dt_data_hora, ds_resultado)
+    VALUES (8, 1, SYSTIMESTAMP, NULL);
+  EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+      v_erro_msg := 'Duplicidade ao inserir imagem';
+    WHEN VALUE_ERROR THEN
+      v_erro_msg := 'Erro ao inserir imagem (por exemplo, NOT NULL violado)';
+    WHEN NO_DATA_FOUND THEN
+      v_erro_msg := 'Nenhum dado encontrado ao inserir imagem';
+    WHEN OTHERS THEN
+      v_erro_msg := 'Erro desconhecido ao inserir imagem';
+  END;
+
+  IF v_erro_msg IS NOT NULL THEN
+    v_erro_msg := SUBSTR(v_erro_msg, 1, 50);
+    INSERT INTO t_erro (nm_erro, dt_ocorrencia, nm_usuario)
+    VALUES (v_erro_msg, SYSDATE, USER);
+  END IF;
+END carregar_imagens;
+
+BEGIN
+  carregar_imagens;
+END;
+
+SELECT * FROM t_erro;
+SELECT * FROM imagem;
+
+
+
+-----------------------PROCEDURE INSERT PARA MEDICO----------------------------
+CREATE OR REPLACE PROCEDURE carregar_medicos AS
+  v_erro_msg VARCHAR2(255);
+BEGIN
+  BEGIN
+    -- Inserir 5 linhas na tabela medico
+    FOR i IN 1..5 LOOP
+      INSERT INTO medico (id_medico, nm_medico, ds_crm, ds_email)
+      VALUES (i, 'Médico ' || TO_CHAR(i), 'CRM' || TO_CHAR(i), 'medico' || TO_CHAR(i) || '@hospital.com');
+    END LOOP;
+  EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+      v_erro_msg := 'Duplicidade ao inserir médico';
+    WHEN VALUE_ERROR THEN
+      v_erro_msg := 'Erro ao inserir médico (por exemplo, NOT NULL violado)';
+    WHEN NO_DATA_FOUND THEN
+      v_erro_msg := 'Nenhum dado encontrado ao inserir médico';
+    WHEN OTHERS THEN
+      v_erro_msg := 'Erro desconhecido ao inserir médico';
+  END;
+
+  IF v_erro_msg IS NOT NULL THEN
+    -- Truncar a mensagem para o tamanho máximo permitido
+    v_erro_msg := SUBSTR(v_erro_msg, 1, 50);
+    INSERT INTO t_erro (nm_erro, dt_ocorrencia, nm_usuario)
+    VALUES (v_erro_msg, SYSDATE, USER);
+  END IF;
+
+  BEGIN
+    -- Tentar inserir um e-mail muito longo em ds_email
+    INSERT INTO medico (id_medico, nm_medico, ds_crm, ds_email)
+    VALUES (6, 'Médico 6', 'CRM6', NULL);
+  EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+      v_erro_msg := 'Duplicidade ao inserir médico';
+    WHEN VALUE_ERROR THEN
+      v_erro_msg := 'Erro ao inserir médico (por exemplo, NOT NULL violado)';
+    WHEN NO_DATA_FOUND THEN
+      v_erro_msg := 'Nenhum dado encontrado ao inserir médico';
+    WHEN OTHERS THEN
+      v_erro_msg := 'Erro desconhecido ao inserir médico';
+  END;
+
+  IF v_erro_msg IS NOT NULL THEN
+    -- Truncar a mensagem para o tamanho máximo permitido
+    v_erro_msg := SUBSTR(v_erro_msg, 1, 50);
+    INSERT INTO t_erro (nm_erro, dt_ocorrencia, nm_usuario)
+    VALUES (v_erro_msg, SYSDATE, USER);
+  END IF;
+END carregar_medicos;
+
+
+BEGIN
+  carregar_medicos;
+END;
+
+SELECT * FROM t_erro;
+SELECT * FROM medico;
--- database_gs_ddl.sql
+++ database_gs_ddl.sql
@@ -1,19 +1,19 @@
 ------------------------TABELA ENDERCO--------------------------------------
 DROP TABLE endereco CASCADE CONSTRAINTS;
 CREATE TABLE endereco (
-    id_endereco    NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id_endereco    NUMBER (4) PRIMARY KEY,
     ds_logradouro  VARCHAR2(50) NOT NULL,
     ds_complemento VARCHAR2(45) NOT NULL,
     nm_cidade      VARCHAR2(55) NOT NULL,
     nm_estado      VARCHAR2(55) NOT NULL,
-    ds_cep         VARCHAR2(8) NOT NULL
+    ds_cep         VARCHAR2(9) NOT NULL
 );
 
 
 -------------------TABELA CONSULTORIO------------------------------
 DROP TABLE CONSULTORIO CASCADE CONSTRAINTS;
 CREATE TABLE CONSULTORIO  (
-    id_consultorio NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id_consultorio NUMBER (4) PRIMARY KEY,
     id_endereco    NUMBER (3) NOT NULL,
     nm_empresa     VARCHAR2(55) NOT NULL,
     nr_cnpj        VARCHAR2(14) NOT NULL
@@ -27,7 +27,7 @@ ALTER TABLE CONSULTORIO
 ----------------------TABELA USUARIO-----------------------
 DROP TABLE usuario CASCADE CONSTRAINTS;
 CREATE TABLE usuario (
-    id_usuario NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id_usuario NUMBER (4) PRIMARY KEY,
     nm_usuario VARCHAR2(55) NOT NULL,
     nr_cpf     VARCHAR2(11) NOT NULL,
     ds_email   VARCHAR2(75) NOT NULL,
@@ -44,7 +44,7 @@ CHECK (upper(ds_genero) = 'M' OR upper(ds_genero) = 'F' OR upper(ds_genero) = 'N
 -------------------TABELA IMAGEM-----------------------------
 DROP TABLE imagem CASCADE CONSTRAINTS;
 CREATE TABLE imagem (
-    id_imagem    NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id_imagem    NUMBER (4) PRIMARY KEY,
     id_usuario   NUMBER(2) NOT NULL,
     dt_data_hora TIMESTAMP NOT NULL,
     ds_resultado VARCHAR2(55) NOT NULL
@@ -60,7 +60,7 @@ ALTER TABLE imagem
 ---------------------TABELA MEDICO----------------------
 DROP TABLE medico CASCADE CONSTRAINTS;
 CREATE TABLE medico (
-    id_medico NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id_medico NUMBER (4) PRIMARY KEY,
     nm_medico VARCHAR2(55) NOT NULL,
     ds_crm    VARCHAR2(55) NOT NULL,
     ds_email  VARCHAR2(65) NOT NULL
@@ -86,7 +86,7 @@ ALTER TABLE pertencer
 ----------------TABELA CONSULTA-------------------------------
 DROP TABLE consulta CASCADE CONSTRAINTS;
 CREATE TABLE consulta (
-    id_consulta   NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id_consulta  NUMBER (4) PRIMARY KEY,
     id_consultorio NUMBER(2) NOT NULL,
     id_medico      NUMBER(2) NOT NULL,
     id_usuario     NUMBER(2) NOT NULL,
@@ -109,7 +109,7 @@ ALTER TABLE consulta
 -----------------TABELA REGISTRO ERRO--------------
 drop table t_erro cascade constraints;
 Create table t_erro(
-    cd_erro number(3),
+    cd_erro NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nm_erro varchar(50),
     dt_ocorrencia date,
     nm_usuario varchar(50)
